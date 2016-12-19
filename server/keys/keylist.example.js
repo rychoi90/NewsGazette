af58c74b6434967543bbb63ee3cf3603db652117
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2016-12-19 01:38:04 -0800

    Add aylien key layout to keylist

--- server/keys/keylist.example.js
+++ server/keys/keylist.example.js
@@ -1,6 +1,10 @@
 //rename this file to keylist.js and include the keys
 
 module.exports = {
-    watsonKey: "INSERT_KEY_HERE_PLEASE",
-    indicoKey: "INSERT_KEY_HERE_PLEASE"
-}
+  watsonKey: 'INSERT_WATSON_KEY_HERE',
+  indicoKey: 'INSERT_INDICO_KEY_HERE',
+  aylienKey: {
+    application_id: 'INSERT_AYLIEN_APP_ID',
+    application_key: 'INSERT_AYLIEN_APP_KEY'
+  }
+};
