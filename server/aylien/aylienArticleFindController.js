7028d98c077103a3d392939295407f7f6f8c6f2e
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2016-12-19 01:32:44 -0800

    Find Articles by keyword or entity, whichever is supplied.

--- server/aylien/aylienArticleFindController.js
+++ server/aylien/aylienArticleFindController.js
@@ -4,28 +4,10 @@ Aylien News API - related news
 For more information, see: https://newsapi.aylien.com/docs/endpoints/related_stories/nodejs
 */
 
-////// convention is to take the res object and append the new data from the middleware then either send it to the next middleware.
-
-// user enters a url into the browser, then starts the extension.
-
-  // the extension will then pull the url from the open tab and then send a post request to the server including that url ====>NEXT()
-
-  // the server router will listen for the post request and upon receiving it will invoke the watsonController.getKeywords function ====>NEXT()
-
-  // getKeywords function will pull the keywords from the article and respond with those keywords. ====>NEXT()
-
-  // articleFinder will take the keywords response and pipe that into the aylienController, to get related articles.
-
-  // aylienController will then ====>NEXT() back to the router so the router can send the response.
-
-
-
-////////////////////////////////////////////////////////////
-
 var Promise = require('bluebird');
 var aylienNewsApi = require('aylien-news-api');
 var aylienNonpromise = new aylienNewsApi.DefaultApi();
-var aylienGoop = require('./aylien_api_key.js');
+var aylienGoop = require('../keys/keylist.js').aylienKey;
 
 
 // GOOP EXAMPLE: {
@@ -45,43 +27,55 @@ app_key.apiKey = aylienGoop.application_key;
 var aylienArticles = Promise.promisifyAll(aylienNonpromise);
 
 module.exports.findRelatedArticles = function(req, res, next) {
+// console.log("IN RELATED ARTICLES");
+  if (!res.compoundContent.entities) {
+    var topKeywords = [];
+    res.compoundContent.keywords.map(function(item) {
+      topKeywords.push(JSON.stringify(item.text));
+    });
+    topKeywords = (topKeywords.join(' OR '));
 
-  // var keyskeys = res.compoundContent['keywords']['keywords'][0].text && res.compoundContent['keywords']['keywords'][1].text && res.compoundContent['keywords']['keywords'][2].text && res.compoundContent['keywords']['keywords'][3].text && res.compoundContent['keywords']['keywords'][4].text;
+  } else if (!!res.compoundContent.entities) {
+    var topEntities = [];
+    res.compoundContent.entities.map(function(item) {
+      if (!!item.disambiguated) {
+        topEntities.push(item.disambiguated.name);
+      } else {
+        topEntities.push(item.text);
+      }
+    });
+    topEntities = topEntities.join(' OR ');
+  }
 
-  // var topKeywords = JSON.stringify(keyskeys);
-
+  var keywordsOrEntities = topEntities || topKeywords;
+  console.log(keywordsOrEntities);
   var opts = {
-    'text' : '"Hillary Clinton" AND "Donald Trump"',
+
+    // 'entities.body.text': keywordsOrEntities,
+    'body': keywordsOrEntities,
     'language': ['en'],
-    'publishedAtStart': 'NOW-1DAY',
+    'publishedAtStart': 'NOW-30DAYS',
+    'publishedAtEnd': 'NOW',
+    // 'cluster': true,
     // 'sentimentBodyPolarity': 'positive',
     // 'sentimentBodyPolarity': 'negative',
-    '_return': ['id', 'title', 'links'],
-    'publishedAtEnd': 'NOW',
-    'categoriesTaxonomy': 'iab-qag',
-    'categoriesId': ['IAB17'],
+    // 'source.name[]': '[!thedailybeast]',
+    '_return': ['id', 'title', 'sentiment', 'links'],
+    // 'categoriesTaxonomy': 'iab-qag',
+    // 'categoriesId': ['IAB17'],
     'perPage': 5
   };
 
 
-  // console.log('inside opts');
-
-  // var callback = function(error, data, response) {
-  //   if (error) {
-  //     console.error(error);
-  //   } else {
-  //     console.log(data);
-  //   }
-  // };
-  // aylienNonpromise.listStories(opts, callback);
-
   if (opts) {
     aylienArticles.listStoriesAsync(opts)
     .then(relatedArticles => {
-      res.compoundContent = res.compoundContent || {};
+      // relatedArticlesTitles = [];
+      // relatedArticlesLinks = [];
 
+      res.compoundContent = res.compoundContent || {};
       res.compoundContent['related'] = relatedArticles;
-      console.log(relatedArticles.stories[0].links);
+      // console.log(res.compoundContent.related, '<<=====RELATED ARTICLES=====');
       next();
     });
   }
