388db64806acd0ae55cf9625da5a82715d071a4a
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2016-12-19 01:36:58 -0800

    Tweak Entity and Keyword search

--- server/watson/watsonController.js
+++ server/watson/watsonController.js
@@ -31,9 +31,9 @@ module.exports.getEmotions = function(req, res, next) {
 };
 
 module.exports.getTitle = function(req, res, next) {
-  // console.log('request body', req.body.url);
+  // console.log('request body', req.body.data.url);
   var parameters = {
-    url: req.body.url
+    url: res.compoundContent.url
   };
   alchemy_language.title(parameters, function (err, response) {
     if (err) {
@@ -50,8 +50,8 @@ module.exports.getTitle = function(req, res, next) {
 
 module.exports.getKeywords = function(req, res, next) {
   var parameters = {
-    url: req.body.url,
-    maxRetrieve: 5
+    url: res.compoundContent.url,
+    maxRetrieve: 3
   };
 
   alchemy_language.keywords(parameters, function (err, response) {
@@ -68,8 +68,8 @@ module.exports.getKeywords = function(req, res, next) {
 
 module.exports.getEntities = function(req, res, next) {
   var parameters = {
-    url: req.body.url,
-    maxRetrieve: 5,
+    url: res.compoundContent.url,
+    maxRetrieve: 3,
     // sourceText: 'cleaned_or_raw'
 
   };
@@ -88,25 +88,25 @@ module.exports.getEntities = function(req, res, next) {
 };
 
 
-module.exports.getRelated = function(req, res, next) {
-  console.log('inside get related');
-  var params = {
-    start: 'now-1M',
-    end: 'now',
-    count: 5,
-    return: 'enriched.url.title'
-  };
+// module.exports.getRelated = function(req, res, next) {
+//   console.log('inside get related');
+//   var params = {
+//     start: 'now-1M',
+//     end: 'now',
+//     count: 5,
+//     return: 'enriched.url.title'
+//   };
 
-  alchemy_data_news.getNews(params, function (err, response) {
-    if (err) {
-      console.log('error:', err);
-    } else {
-      console.log('inside getnews');
-      res.compoundContent = res.compoundContent || {};
-      res.compoundContent['relatedArticles'] = response;
-      console.log('gotpast res');
+//   alchemy_data_news.getNews(params, function (err, response) {
+//     if (err) {
+//       console.log('error:', err);
+//     } else {
+//       console.log('inside getnews');
+//       res.compoundContent = res.compoundContent || {};
+//       res.compoundContent['relatedArticles'] = response;
+//       console.log('gotpast res');
 
-    }
-    next();
-  });
-};
+//     }
+//     next();
+//   });
+// };
