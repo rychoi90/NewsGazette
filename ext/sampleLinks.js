f4199ba87ca61ed1b4479443f0633c43450f9ac6
Ryan Yoon Bum Choi <ryanychoi90@gmail.com>
2017-05-10 13:44:22 -0700

    update sample links with news logo

--- ext/sampleLinks.js
+++ ext/sampleLinks.js
@@ -1,9 +1,10 @@
 var sampleLinks = {
   stories: [
     {
-      title: 'To Russia with love: Trump’s dreams of Kremlin power',
+      title: 'Despite Recusal Pledge, Sessions Interviewing Candidates For Interim FBI Director',
+      logo: 'npr.png',
       links: {
-        permalink: 'http://www.timesofisrael.com/to-russia-with-love-trumps-dreams-of-kremlin-power/',
+        permalink: 'http://www.timesofisrael.com/to-russia-with-love-trumps-dreams-of-kremlin-power',
       },
       sentiment: {
         'title': {
@@ -16,8 +17,9 @@ var sampleLinks = {
         }
       }
     },
-    {
+  {
       title: 'China "will not exercise restraint" if Trump continues to attack Beijing, state-run paper warns',
+      logo: 'npr.png',
       links: {
         permalink: 'http://www.ibtimes.co.uk/china-will-not-exercise-restraint-if-trump-continues-attack-beijing-state-run-paper-warns-1597127',
       },
@@ -34,6 +36,7 @@ var sampleLinks = {
     },
     {
       title: 'Beijing sees US drone as part of South China Sea spying efforts',
+      logo: 'npr.png',
       links: {
         permalink: 'http://timesofindia.indiatimes.com/world/china/beijing-sees-us-drone-as-part-of-south-china-sea-spying-efforts/articleshow/56058713.cms',
       },
@@ -50,6 +53,7 @@ var sampleLinks = {
     },
     {
       title: 'Rhode Island electors to cast votes for president Monday',
+      logo: 'npr.png',
       links: {
         permalink: 'http://www.richmond.com/news/ap/article_f52c821a-4271-565f-ab33-98aa07dc7b21.html',
       },
@@ -66,6 +70,7 @@ var sampleLinks = {
     },
     {
       title: 'Mossad chief gives secret intelligence briefing to Trump staff',
+      logo: 'npr.png',
       links: {
         permalink: 'http://www.israelnationalnews.com/News/News.aspx/221929',
       },
@@ -82,6 +87,7 @@ var sampleLinks = {
     },
     {
       title: 'Connecticut electors set to cast votes for Clinton',
+      logo: 'npr.png',
       links: {
         permalink: 'http://www.richmond.com/news/ap/article_46125fd4-a36c-5702-adb4-ff484b2a5684.html',
       },
